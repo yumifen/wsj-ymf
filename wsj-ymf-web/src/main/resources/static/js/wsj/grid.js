5b9565e625d0f1391c23665d2956d65eded2717d
yumifen <yuzhaopeng@foxmail.com>
2018-11-26 11:51:42 +0800

    除checkbox组件，其余的功能都已经实现

--- wsj-ymf-web/src/main/resources/static/js/wsj/grid.js
+++ wsj-ymf-web/src/main/resources/static/js/wsj/grid.js
@@ -1,6 +1,6 @@
- function Grid(options){                    //js 调用生成grid实例，初始化grid配置
-     var this_ = this;
-     this_.gridOptions= {             //grid默认配置
+function Grid(options){                    //js 调用生成grid实例，初始化grid配置
+    var this_ = this;
+    this_.gridOptions= {             //grid默认配置
         href:'',                      //grid请求地址
         pagegation:true,             //是否分页
         params:{},                    //请求参数
@@ -12,6 +12,7 @@
         content:[],                 //用来存储index的元素<< 1 2 3 4 5 >>
         stepLenth:5,               //定义存储index的大小
         vueId:'',                  //与VUE进行绑定的id
+        sizeArray:[10,15,20,25],
         callBack:function(dataArray,data){           //grid加载后的回调函数
 
         },
@@ -22,18 +23,23 @@
     this_.gridOptions = $.extend({},this_.gridOptions,options); //重新加载配置
 
     var opt =  this_.gridOptions;
-    content = new Array(opt.stepLenth);
+    opt.content = new Array(opt.stepLenth).fill(0);
 
     //定义vue的模型
     this_.vueDataOptions = {
-            dataArray:[],
-            pageSize :0,
-            totalSize :0,
-            maxPage :0,
-            currentPage: opt.currentPage,
-            leftTag : opt.leftFlag,
-            rightTag : opt.rightFlag,
-            number:[]
+        dataArray:[],
+        pageSize :0,
+        totalSize :0,
+        maxPage :0,
+        currentPage: opt.currentPage,
+        leftTag : opt.leftFlag,
+        rightTag : opt.rightFlag,
+        number:[],
+        sizeArray:opt.sizeArray,
+        srow:0,
+        erow:0,
+        lastNum:0,
+        nextNum:0
     }
 
     this_.vueMethodOptions = {
@@ -46,139 +52,183 @@
         lastPage : function(){             //跳转到最后一页
             var maxPage =  parseInt(this.totalSize / this.pageSize);
             rload(this_,maxPage);
+        },
+        sizeLoad:function(pageSize){
+            console.log(pageSize);
+            this_.gridOptions.pageSize = pageSize;
+            rload(this_,1);
         }
     }
-    this_.vueMethodOptions = $.extend({},this_.gridOptions.extendVueMS,options); //重新加载配置
+    //this_.vueMethodOptions = $.extend({},this_.gridOptions.extendVueMS,options); //重新加载配置
 
-     this_.vueDataOptions.number=[];
-     $.each(this_.gridOptions.content, function(i, value) {
-         this_.vueDataOptions.number.push(value);
-     })
+    console.log("v-grid的model当前为："+this_.gridOptions.vueId);
 
-     console.log("v-grid的model当前为："+this_.gridOptions.vueId);
-     this.gvue = new Vue({
-         el:"#"+this_.gridOptions.vueId,
-         data:this_.vueDataOptions,
-         methods:this_.vueMethodOptions,
-         computed:this_.gridOptions.extentdVueCS,
-         components:{                                                                                       //在组件中定义分页toolbar
-             "pagegation-template":{
-                 template:'<div class="row">' +
-                                 '<div class="col-sm-6">' +
-                                     '<div class="dataTables_info"   role="alert" aria-live="polite" aria-relevant="all">显示 1 到 10 项，共 57 项</div>' +
-                                 '</div>' +
-                                 '<div class="col-sm-6">' +
-                                     '<div class="dataTables_paginate paging_simple_numbers" >' +
-                                         '<ul class="pagination">' +
-                                                 '<li class="paginate_button previous disabled" aria-controls="editable" tabindex="0"><a href="#">上一页</a></li>' +
-                                                 '<li class="paginate_button active" aria-controls="editable" tabindex="0"><a href="#">1</a></li>' +
-                                                 '<li class="paginate_button " aria-controls="editable" tabindex="0"><a href="#">2</a></li>' +
-                                                 '<li class="paginate_button " aria-controls="editable" tabindex="0"><a href="#">3</a></li>' +
-                                                 '<li class="paginate_button " aria-controls="editable" tabindex="0"><a href="#">4</a></li>' +
-                                                 '<li class="paginate_button " aria-controls="editable" tabindex="0"><a href="#">5</a></li>' +
-                                                 '<li class="paginate_button " aria-controls="editable" tabindex="0"><a href="#">6</a></li>' +
-                                                 '<li class="paginate_button next" aria-controls="editable" tabindex="0" ><a href="#">下一页</a></li>' +
-                                         '</ul>' +
-                                     '</div>' +
-                                 '</div>' +
-                              '</div>'
-             },
-             "rows-template":{
-                 // template:'<div class="row">' +
-                 //                 '<div class="col-sm-6">' +
-                 //                                 '<div class="dataTables_length">' +
-                 //                                     '<label>每页 <select name="DataTables_Table_0_length" aria-controls="DataTables_Table_0" class="form-control input-sm">' +
-                 //                                         '<option value="10">10</option>' +
-                 //                                         '<option value="25">25</option>' +
-                 //                                         '<option value="50">50</option>' +
-                 //                                         '<option value="100">100</option'>+
-                 //                                     '</select> 条记录</label>' +
-                 //                                 '</div>' +
-                 //                 '</div>'+
-                 //             '</div>'
-                 template:'<div class="row">' +
-                               '<div class="col-sm-6">' +
-                                    '<div class="dataTables_length" id="DataTables_Table_0_length"><label>每页 ' +
-                                        '<select name="DataTables_Table_0_length" aria-controls="DataTables_Table_0" class="form-control input-sm">' +
-                                             '<option value="10">10</option>' +
-                                             '<option value="15">15</option>' +
-                                             '<option value="20">20</option>' +
-                                             '<option value="25">25</option>' +
-                                        '</select> 条记录</label>' +
-                                    '</div>' +
-                               '</div>' +
-                            '</div>'
+    this_.gvue = null;
 
-             }
-         },
-         watch:{
-             "currentPage":{
-                 handler:function(val,oldVal){
-                     console.log("监听到了currentPage数据变化--------》将变化为第"+val+"页");
-                 },
-                 //deep:true,
-             },
-             "pageSize":{
-                 handler:function(val,oldVal){
-                     console.log("监听到了pageSize数据变化--------》将变化为一页为："+val);
-                 },
-                 //deep:true,
-             },
-         }
-     });
- }
+    this_.ptemplate = {
+                        props:['currentPage','leftTag','rightTag','maxPage','number','totalSize','srow','erow'],
+                        template:'<div class="row">' +
+                        '<div class="col-sm-6">' +
+                        '<div class="dataTables_info"   role="alert" >显示 {{this.$parent.srow}} 到 {{this.$parent.erow}} 项，共 {{this.$parent.totalSize}} 项</div>' +
+                        '</div>' +
+                        '<div class="col-sm-6">' +
+                        '<div class="dataTables_paginate paging_simple_numbers" >' +
+                        '<ul class="pagination">' +
+                        '<li class="paginate_button previous " v-bind:class="{disabled: this.$parent.currentPage==1?true:false}"  @click="goPage(($parent.currentPage)-1)"><a href="#">上一页</a></li>' +
+                        '<li class="paginate_button"  v-bind:class="{active: num==currentPage?true:false}" @click="goPage(num)" v-for="num in this.$parent.number" v-if = "num != 0"><a href="#">{{num}}</a></li>' +
+                        '<li class="paginate_button next" v-bind:class="{disabled: this.$parent.currentPage==this.$parent.maxPage?true:false}" @click="goPage(($parent.currentPage)+1)"><a href="#">下一页</a></li>' +
+                        '</ul>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>',
+                        // data:function(){
+                        //     return {
+                        //         currentPage: this_.vueDataOptions.currentPage,
+                        //         leftTag:     this_.vueDataOptions.leftTag,
+                        //         rightTag:    this_.vueDataOptions.rightTag,
+                        //         maxPage:     this_.vueDataOptions.maxPage,
+                        //         number:      this_.vueDataOptions.number,
+                        //         totalSize:  this_.vueDataOptions.totalSize,
+                        //         srow:        this_.vueDataOptions.srow,
+                        //         erow:        this_.vueDataOptions.erow
+                        //     }
+                        // },
+                        methods:this_.vueMethodOptions,
+                        watch: {
+                            number: {
+                                handler(newValue, oldValue) {
+                                    for (var i = 0; i < newValue.length; i++) {
+                                        if (oldValue[i] != newValue[i]) {
+                                            console.log(newValue)
+                                        }
+                                    }
+                                },
+                                deep: true
+                            },
+                            srow(newValue, oldValue) {
+                                console.log(newValue)
+                            },
+                            erow(newValue, oldValue) {
+                                console.log(newValue)
+                            },
+                        }
+
+                    };
+    this_.rtemplate = {
+                        //props:['sizeArray'],
+                        template:'<div class="row">' +
+                        '<div class="col-sm-6">' +
+                        '<div class="dataTables_length" id="DataTables_Table_0_length"><label>每页 ' +
+                        '<select name="DataTables_Table_0_length" aria-controls="DataTables_Table_0" v-model="sizeVal"  class="form-control input-sm">' +
+                        '<option :value="size" v-for="size in this.$parent.sizeArray"  :key="size">{{size}}</option>' +
+                        '</select> 条记录</label>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>',
+                         methods:this_.vueMethodOptions,
+                        data:function(){
+                            return {
+                                "sizeVal":''
+                            }
+                        },
+                        watch:{
+                            sizeVal(val,oladVal){
+                                 this.sizeLoad(val);
+                            }
+                        }
+    };
+}
 
 var rbing = function(this_){
-        this_.gvue.data=this_.vueDataOptions;
-        this_.gvue.methods=this_.vueMethodOptions;
-        this_.gvue.computed=this_.gridOptions.extentdVueCS;
+
+    this_.gvue =  new Vue({
+        el:"#"+this_.gridOptions.vueId,
+        data:this_.vueDataOptions,
+        methods:this_.gridOptions.extendVueMS,
+        //methods:this_.vueMethodOptions,
+        computed:this_.gridOptions.extentdVueCS,
+        components:{                                                                                       //在组件中定义分页toolbar
+            "pagegation-template": this_.ptemplate,
+            "rows-template":this_.rtemplate
+        },
+        watch:{
+            "currentPage":{
+                handler:function(val,oldVal){
+                    console.log("监听到了currentPage数据变化--------》将变化为第"+val+"页");
+                },
+                //deep:true,
+            },
+            "pageSize":{
+                handler:function(val,oldVal){
+                    console.log("监听到了pageSize数据变化--------》将变化为一页为："+val);
+                },
+                //deep:true,
+            },
+        }
+    });
 }
 
 var rbuild = function(this_){
-        var data=this_.gridOptions;
-        var maxPage =  parseInt(data.totalSize / data.pageSize)+((data.totalSize % data.pageSize)>0?1:0);
-        if(maxPage <= data.content.length){//当最大页码小于数组长度
-            for(var i =1 ;i<= data.content.length;i++){
-                if(i<= maxPage)
-                    data.content[i-1] = i;
-                else
-                    data.content[i-1] = 0;
-            }
-            data.leftFlag = false;
-            data.rightFlag = false;
-
-        }else{//当最大页码大于数组长
-            var pagePoint=0
-            var diff=parseInt(data.content.length /2);
-            var mo=data.content.length %2;
-            if(data.currentPage+data.content.length>maxPage){
-                data.leftFlag=true;
-                pagePoint=data.currentPage+ diff+mo;
-                if(pagePoint>=maxPage){
-                    pagePoint=maxPage-data.content.length+mo;
-                    data.rightFlag = false;
-                }else{
-                    data.rightFlag = true;
-                    pagePoint=pagePoint-data.content.length;
-                }
-
-            }else{
-                data.rightFlag=true;
-                pagePoint=data.currentPage- diff;
-            }
-            //修正一下值，如果最小值<=0
-            if(pagePoint<=0){
-                pagePoint=1;
-                data.leftFlag = false;
-            }else{
-                data.leftFlag=true;
-            }
-            //将计算的结果写回数组
-            for(var i =1;i<= data.content.length;i++,pagePoint++){
-                data.content[i-1] = pagePoint;
-            }
+    var data=this_.gridOptions;
+    var maxPage =  parseInt(data.totalSize / data.pageSize)+((data.totalSize % data.pageSize)>0?1:0);
+    if(maxPage <= data.content.length){//当最大页码小于数组长度
+        for(var i =1 ;i<= data.content.length;i++){
+            if(i<= maxPage)
+                data.content[i-1] = i;
+            else
+                data.content[i-1] = 0;
         }
+        data.leftFlag = false;
+        data.rightFlag = false;
+
+    }else{//当最大页码大于数组长
+        var pagePoint=0
+        var diff=parseInt(data.content.length /2);
+        var mo=data.content.length %2;
+        if(data.currentPage+data.content.length>maxPage){
+            data.leftFlag=true;
+            pagePoint=data.currentPage+ diff+mo;
+            if(pagePoint>=maxPage){
+                pagePoint=maxPage-data.content.length+mo;
+                data.rightFlag = false;
+            }else{
+                data.rightFlag = true;
+                pagePoint=pagePoint-data.content.length;
+            }
+
+        }else{
+            data.rightFlag=true;
+            pagePoint=data.currentPage- diff;
+        }
+        //修正一下值，如果最小值<=0
+        if(pagePoint<=0){
+            pagePoint=1;
+            data.leftFlag = false;
+        }else{
+            data.leftFlag=true;
+        }
+        //将计算的结果写回数组
+        for(var i =1;i<= data.content.length;i++,pagePoint++){
+            data.content[i-1] = pagePoint;
+        }
+    }
 }
+
+var dbind = function(this_) {
+
+    // Vue.set(this_.gvue,"data",this_.vueDataOptions) //这样就能被vue监控到，更新视图
+    // Vue.set(this_.gvue,"methods",this_.vueMethodOptions) //这样就能被vue监控到，更新视图
+    // Vue.set(this_.gvue,"computed",this_.gridOptions.extentdVueCS) //这样就能被vue监控到，更新视图
+    // Vue.set(this_.gvue,"pagegation-template",this_.ptemplate) //这样就能被vue监控到，更新视图
+    // Vue.set(this_.gvue,"rows-template",this_.rtemplate) //这样就能被vue监控到，更新视图
+
+    // this_.gvue.data=this_.vueDataOptions;
+    // this_.gvue.methods=this_.vueMethodOptions;
+    // this_.gvue.computed=this_.gridOptions.extentdVueCS;
+    // this_.gvue["pagegation-template"]=this_.ptemplate;
+    // this_.gvue["rows-template"]= this_.rtemplate;
+}
+
 
 var rload = function(this_,pageNumber){
 
@@ -205,22 +255,29 @@ var rload = function(this_,pageNumber){
             }
             gridOptions.pageSize=result.currSize ;
             gridOptions.totalSize=result.totalCount ;
-
+            gridOptions.currentPage=result.currNum;
             rbuild(this_);    //重新配置<< 1 2 3 4 5 >>
 
             this_.vueDataOptions.number=[];
             $.each(this_.gridOptions.content, function(i, value) {
                 this_.vueDataOptions.number.push(value);
             })
-
             this_.vueDataOptions.currentPage=gridOptions.currentPage;
             this_.vueDataOptions.leftTag=gridOptions.leftFlag;
             this_.vueDataOptions.rightTag=gridOptions.rightFlag;
-            this_.vueDataOptions.pageSize=gridOptions.pageSize;
+            this_.vueDataOptions.pageSize= gridOptions.pageSize;
             this_.vueDataOptions.totalSize=gridOptions.totalSize;
-            this_.vueDataOptions.maxPage=parseInt(gridOptions.totalSize / gridOptions.pageSize)+((gridOptions.totalSize % gridOptions.pageSize)>0?1:0);
-
-            rbing(this_);
+            this_.vueDataOptions.maxPage=parseInt(result.totalCount / result.currSize)+((result.totalCount % result.currSize)>0?1:0);
+            if(result.grids!=null){
+                this_.vueDataOptions.srow = result.currNum>1?(result.currNum)*(result.currSize)+1:1;
+                var length = result.grids.length;
+                this_.vueDataOptions.erow = result.currNum>1?(result.currNum)*(result.currSize)+length:length;
+            }
+            if(this_.gvue!=null){
+                dbind(this_);
+            }else{
+                rbing(this_);
+            }
         },
         params:$.extend({},{
             "pageNum":pageNumber,
@@ -231,25 +288,25 @@ var rload = function(this_,pageNumber){
 
 }
 
- //渲染表格
- Grid.prototype.rendGrid=function(options){              //options为url参数
-     //取json数据
-     var this_=this;
-     this_.gridOptions.params=$.extend({},options);
-     var opts =this_.gridOptions.params
-     if(this_.gridOptions.href && this_.gridOptions.href!=''){
-         rload(this_,1); //1 为初始化页码为第一页
-     }
- }
+//渲染表格
+Grid.prototype.rendGrid=function(options){              //options为url参数
+    //取json数据
+    var this_=this;
+    this_.gridOptions.params=$.extend({},options);
+    var opts =this_.gridOptions.params
+    if(this_.gridOptions.href && this_.gridOptions.href!=''){
+        rload(this_,1); //1 为初始化页码为第一页
+    }
+}
 
- Grid.prototype.refresh=function(){
-     var this_=this;
-     //取json数据
-     return this_.rendGrid(this_.gridOptions.params );
- }
+Grid.prototype.refresh=function(){
+    var this_=this;
+    //取json数据
+    return this_.rendGrid(this_.gridOptions.params );
+}
 
- Grid.prototype.getParams=function(){
-     var this_=this;
-     return this_.gridOptions.params;
- }
+Grid.prototype.getParams=function(){
+    var this_=this;
+    return this_.gridOptions.params;
+}
 
